663ea46c21fa0f84f2c9d886ca77a79add8d42d4
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-07-10 12:09:03 +0530

    initial commit

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log("Successfully connected to MongoDB");
+	} catch (error) {
+		console.error("Error connecting to MongoDB:", error);
+	}
+};
+
+export default connectToMongoDB
